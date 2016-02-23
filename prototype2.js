8007770391aca87079f26c5d426b1b61a4138853
李诺 <linuo@linuodeMacBook-Air.local>
2016-02-23 17:40:30 +0800

    prototype

---
+++ prototype2.js
@@ -0,0 +1,9 @@
+/*
+	没有原型的构造函数
+*/
+function Dog(name){
+	this.name = name;
+	this.species = "animal";
+}
+
+
---
+++ prototype3.js
@@ -0,0 +1,8 @@
+/*
+	引入原型后的对象
+*/
+function Dog(name){
+	this.name = name;
+}
+
+Dog.prototype.species = "animal";
